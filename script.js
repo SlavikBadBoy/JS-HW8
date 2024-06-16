9f1814b3718718abcf3f8dd2b0d7c346e13b57db
SlavikBadBoy <buracenkoslavik@gmail.com>
2024-06-16 12:42:13 +0300

    8

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 "use strict";
-// const friends = ["Mango", "Poly", "Kiwi", "Ajax"];
-// let string = "";
-// for (let friend of friends) {
-//   if (string) string += ", ";
-//   string += friend;
-// }
-// const friends = ["Mango", "Poly", "Kiwi", "Ajax"];
-// const string = friends.join(", ");
-// console.log(string);
+const friends = ["Mango", "Poly", "Kiwi", "Ajax"];
+let string = "";
+console.log(friends.join(", "));
+for (let friend of friends) {
+  string = string + friend;
+
+  if (friend !== "Ajax") {
+    string += ", ";
+  }
+}
+console.log(string);
 
 const cards = [
   "Карточка-1",
@@ -16,17 +18,15 @@ const cards = [
   "Карточка-4",
   "Карточка-5",
 ];
-// const cardToUpdate = "Карточка-4";
-
-// const index = cards.indexOf(cardToUpdate);
-// console.log(index);
 const cardToRemove = "Карточка-3";
 const indexToRemove = cards.indexOf(cardToRemove);
 cards.splice(indexToRemove, 1);
+console.log(cards);
+
 const cardToInsert = "Карточка-6";
-const indexToInsert = 2;
-cards.splice(indexToInsert, 0, cardToInsert);
+cards.push(cardToInsert);
+console.log(cards);
+
 const cardToUpdate = "Карточка-4";
-const indexToUpdate = 3;
-cards.splice(indexToUpdate, 1, "Карточка-4");
+const indexToUpdate = cards.indexOf("Карточка-4");
 console.log(cards);
